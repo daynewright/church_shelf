e9963a951d5b930f95ac1b7be7ba0f17066493e9
Dayne Wright <daynewr@gmail.com>
2024-08-20 22:50:18 -0500

    fix: correct mobile menu

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -2,11 +2,15 @@ import { Controller } from "@hotwired/stimulus";
 import { leave, toggle } from "el-transition";
 
 export default class extends Controller {
-  static targets = ["openMenu", "menu"];
+  static targets = ["openMenu", "menu", "openMobileMenu", "mobileMenu"];
 
   connect() {
     this.openMenuTarget.addEventListener("click", (e) => {
       toggle(this.menuTarget);
+    });
+
+    this.openMobileMenuTarget.addEventListener("click", (e) => {
+      toggle(this.mobileMenuTarget);
     });
   }
 }
